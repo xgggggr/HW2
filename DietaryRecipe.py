2352637470e949bb7c17c1a623c9934473f47580
xgggggr <a.tleuzh07@mail.ru>
2026-06-02 17:14:03 +0300

    реализовал класс DietaryRecipe

---
+++ DietaryRecipe.py
@@ -0,0 +1,14 @@
+from Recipe import Recipe
+
+
+class DietaryRecipe(Recipe):
+    def __init__(self, title, diet_type, ingredients=None):
+        if ingredients is None:
+            ingredients = []
+        super().__init__(title, ingredients)
+        self._diet_type = diet_type
+    def scale(self, ratio: float):
+        new_recipe = super().scale(ratio)
+        return DietaryRecipe(self._title, self._diet_type, new_recipe._ingredients)
+    def __str__(self):
+        return f"[{self._diet_type}] " + super().__str__()
